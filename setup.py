c01745b5120ba3f1ba10f7ec3d3d82dc170694f3
douglasfariasil <douglasfariasil@outlook.com>
2025-05-21 21:56:20 -0300

    Adicionado arquivos

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='financa_py',
+    version='0.0.1',
+    license='MIT License',
+    author='Douglas Faria',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='douglasfariasil14@gmail.com',
+    keywords='financa_py',
+    description=u'este é meu primeiro projeto no pypi',
+    packages=['financa_py'],
+    )
